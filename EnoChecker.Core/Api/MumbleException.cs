84f85fb7eeb609171dd9f78cb8e454ec63819311
Benedikt Radtke <benediktradtke@gmail.com>
2021-06-27 00:04:26 +0200

    exodus from enoengine

---
+++ EnoChecker.Core/Api/MumbleException.cs
@@ -0,0 +1,16 @@
+﻿namespace EnoCore.Checker
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class MumbleException : Exception
+    {
+        public MumbleException(string scoreboardMessage)
+            : base(scoreboardMessage)
+        {
+        }
+    }
+}
---
+++ EnoChecker.Core/Api/OfflineException.cs
@@ -0,0 +1,16 @@
+﻿namespace EnoCore.Checker
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class OfflineException : Exception
+    {
+        public OfflineException(string scoreboardMessage)
+            : base(scoreboardMessage)
+        {
+        }
+    }
+}
